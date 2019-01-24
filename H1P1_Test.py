a15d5c43fd11fee1b31c9ffd80c55112f213af80
sagrawal19 <46700824+sagrawal19@users.noreply.github.com>
2019-01-24 12:27:43 -0800

    delete

--- H1P1_Test.py
+++
@@ -1,6 +0,0 @@
-Test11first_name = input("Please enter your first name:")
-last_name = input("Please enter your last name:")
-age = input("please enter your age:")
-confidence = input("Please enter your confidence in programmer between 0-100%:")
-dog_age = int(age)*7
-print("Hello ",Test11first_name ," ",last_name, ",nice to meet you!\nYou might be ",age," in human years,but in dog years you are ",dog_age)
