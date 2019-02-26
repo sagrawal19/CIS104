8168248162cd41b0be5c406e518bc2efaac7fd8e
sagrawal19 <46700824+sagrawal19@users.noreply.github.com>
2019-02-26 13:48:30 -0800

    Exception

--- calculator/main.py
+++ calculator/main.py
@@ -13,57 +13,68 @@ print("i.Invert")
 print("c.Clear")
 print("x.Exit")
 
-# while loop for calculation
+
+    # while loop for calculation
 while(True):
-    
-    choice = input("Enter choice(1/2/3/4/5/r/i/c/x):")
+        
+        choice = input("Enter choice(1/2/3/4/5/r/i/c/x):")
 
-    if choice == 'r':
-       print(calculator.getMemoryValue())
-       continue
-    elif choice == 'i':  
-        print(calculator.invert())
-        continue
-    elif choice == 'c':   
-        print(calculator.clearMemory())  
-        continue
-    elif choice == 'x':
-        break
-    
-    if choice != '1' and choice != '2' and choice != '3' and choice != '4'and choice != '5'and choice != 'r' and choice != 'i'and choice != 'c':  
-        print("Invalid choice")
-        continue
-    if operator == '':
-        leftValue = int(input("Enter first number: "))
-    else:
-        leftValue = calculator.getMemoryValue()  
-    operator = choice  
-    
-    rightValue = int(input("Enter second number: "))
+        if choice == 'r':
+            print(calculator.getMemoryValue())
+            continue
+        elif choice == 'i':  
+            print(calculator.invert())
+            continue
+        elif choice == 'c':   
+            print(calculator.clearMemory())  
+            continue
+        elif choice == 'x':
+            break
+        
+        if choice != '1' and choice != '2' and choice != '3' and choice != '4'and choice != '5'and choice != 'r' and choice != 'i'and choice != 'c':  
+            print("Invalid choice")
+            continue
+        try:       
+            if operator == '':
+                strleftValue = ""
+                while(not strleftValue.isdigit()):
+                    strleftValue = input("Enter first number: ")
+                leftValue = int(strleftValue)
+                
+            else:
+                leftValue = calculator.getMemoryValue()  
+            operator = choice  
+            strRightValue =""
+            while(not strRightValue.isdigit()):
+                strRightValue = input("Enter second number: ")
+            rightValue = int(strRightValue)
+        except ValueError:
+            print("No.. input string is not a number. It's a string")
+        if choice == '1':
+            currentValue = calculator.add(leftValue,rightValue)
+            print(leftValue,"+",rightValue,"=",currentValue)
+            calculator.setMemoryValue(currentValue)
+            print (calculator.getMemoryValue())
 
-    if choice == '1':
-        currentValue = calculator.add(leftValue,rightValue)
-        print(leftValue,"+",rightValue,"=",currentValue)
-        calculator.setMemoryValue(currentValue)
-        print (calculator.getMemoryValue())
+        elif choice == '2':
+            currentValue = calculator.subtract(leftValue,rightValue)
+            print(leftValue,"-",rightValue,"=",currentValue)
+            calculator.setMemoryValue(currentValue)
 
-    elif choice == '2':
-        currentValue = calculator.subtract(leftValue,rightValue)
-        print(leftValue,"-",rightValue,"=",currentValue)
-        calculator.setMemoryValue(currentValue)
+        elif choice == '3':
+            currentValue = calculator.multiply(leftValue,rightValue)
+            print(leftValue,"*",rightValue,"=",currentValue)
+            calculator.setMemoryValue(currentValue)
 
-    elif choice == '3':
-        currentValue = calculator.multiply(leftValue,rightValue)
-        print(leftValue,"*",rightValue,"=",currentValue)
-        calculator.setMemoryValue(currentValue)
+        elif choice == '4':
+            currentValue = calculator.divide(leftValue,rightValue)
+            print(leftValue,"/",rightValue,"=",currentValue)
+            calculator.setMemoryValue(currentValue)
+        elif choice == '5':
+            currentValue = calculator.power(leftValue,rightValue)
+            print(leftValue,"^",rightValue,"=",currentValue)
+            calculator.setMemoryValue(currentValue)    
+        else:
+            print("Invalid input")
 
-    elif choice == '4':
-        currentValue = calculator.divide(leftValue,rightValue)
-        print(leftValue,"/",rightValue,"=",currentValue)
-        calculator.setMemoryValue(currentValue)
-    elif choice == '5':
-        currentValue = calculator.power(leftValue,rightValue)
-        print(leftValue,"^",rightValue,"=",currentValue)
-        calculator.setMemoryValue(currentValue)    
-    else:
-        print("Invalid input")
+            
